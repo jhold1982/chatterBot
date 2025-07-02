b1027930f48f2cd33d17a904a389aa76cfb06838
Justin Hold <36420615+jhold1982@users.noreply.github.com>
2025-07-02 13:42:06 -0500

    Initial Commit

---
+++ chatterBot/chatterBotApp.swift
@@ -0,0 +1,17 @@
+//
+//  chatterBotApp.swift
+//  chatterBot
+//
+//  Created by Justin Hold on 7/2/25.
+//
+
+import SwiftUI
+
+@main
+struct chatterBotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
